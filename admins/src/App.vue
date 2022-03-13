1ae55703623a9ced0344927fbe7e18319417e88f
‘wanghuahua’ <2417297008@qq.com>
2022-03-13 13:06:41 +0800

    1

--- admins/src/App.vue
+++ admins/src/App.vue
@@ -3,10 +3,15 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <div class="hua">dsjdkjdsjd</div>
   <router-view />
 </template>
+<script>
+// import "@/assets/css/1.css";
+</script>
 
 <style>
+@import "@/assets/css/1.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,5 +22,9 @@
 
 nav {
   padding: 30px;
+}
+
+.hua {
+  color: var(--my);
 }
 </style>
