9f1b4714b5a13e86cd4c5df558d958dc210ee4be
wangxin <2417297008@qq.com>
2022-03-28 17:47:21 +0800

    fix

--- admins/postcss.config.js
+++ admins/postcss.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   plugins: {
     "postcss-pxtorem": {
-      rootValue: 37.5,
-      propList: ["*"],
+      rootValue: 13.36,  //设计稿是根据1366 高度
+      propList: ["*", '!border'],
+      unitPrecision: 2, //保留rem 小数点几位
+      minPixeIValue: 12
+
     },
   },
 };
